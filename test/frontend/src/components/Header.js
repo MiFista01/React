02062cac1e1f4f37e448513478cc0344ae8b3c07
pupil <pupil@ivkhk.ee>
2023-05-04 15:21:51 +0300

    aaaaa

---
+++ test/frontend/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../logo.svg';
+import { Container, Nav, NavLink, Navbar } from 'react-bootstrap'
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+export default function Header(){
+    return(
+        <header>
+        <Navbar sticky='top' collapseOnSelect expand="md" bg='dark' variant='dark'>
+            <Container>
+                <Navbar.Brand>
+                    <NavLink href='/'><img src={logo} className="App-logo" alt="logo" />Test</NavLink>
+                </Navbar.Brand>
+            
+            <NavbarToggle aria-controls='responsive-navbar-nav' />
+            <NavbarCollapse id='responsive-navbar-nav'>
+                <Nav className='me-auto'>
+                    <NavLink href='/specialties'>Specialties</NavLink>
+                </Nav>
+            </NavbarCollapse>
+            </Container>
+        </Navbar>
+        </header>
+    )
+}
